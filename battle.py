afb92392e65d53c9a3866fee2ae0e300d6c2054d
mxdls <mxdls@live.com>
2017-08-02 00:11:21 +0800

    add something

--- battle.py
+++ battle.py
@@ -4,6 +4,7 @@ from tool import *
 from dbcontroller import *
 
 
+# 游戏种类:1 自定义 2人机？ 3匹配赛 4单双排 5灵活组排 6大乱斗
 class battlelist(object):
     def __init__(self, userid='4006716018', zonepy='dx7', user=None):
         self.userid = userid
@@ -14,20 +15,28 @@ class battlelist(object):
             self.userid = user.userid
             self.zonepy = user.zonepy
 
-    def update_list(self):
+    def update_list(self, n=-1):
         r = requests.get(get_battle_list_url(self.zonepy, self.userid))
         j = res_to_dic(r)
         for x in j["game_list"]:
             self.battleid_list.append(x["game_id"])
-            b = battle(self.zonepy, self.userid, x["game_id"])
+            b = battle(self.zonepy, self.userid, x["game_id"], x["game_type"]["const"])
             self.battle_list.append(b)
+            n -= 1
+            if (n == 0): break
+
+    def insert_battles(self):
+        for x in self.battle_list:
+            insert_battle(x)
+        print("插入完毕")
 
 
 class battle(object):
-    def __init__(self, zonepy, userid, battleid):
+    def __init__(self, zonepy, userid, battleid, type):
         self.zonepy = zonepy
         self.userid = userid
         self.battleid = battleid
+        self.type = type
         r = requests.get(get_battle_detail_url(self.zonepy, self.userid, self.battleid))
         with open("./battle_details/%s.json" % (str(self.zonepy) + "-" + str(self.battleid)), 'w',
                   encoding='utf-8') as json_file:
@@ -40,4 +49,4 @@ class battle(object):
 if __name__ == "__main__":
     b = battlelist()
     b.update_list()
-    print(len(b.battle_list))
+    b.insert_battles()
--- dbcontroller.py
+++ dbcontroller.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, create_engine, Integer, Float
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 # 连接信息
@@ -13,8 +13,8 @@ class User(Base):
     # 表的名字:
     __tablename__ = 'user'
     # 表的结构:
-    userid = Column(String(20), primary_key=True)
-    username = Column(String(20))
+    userId = Column(String(20), primary_key=True)
+    userName = Column(String(20))
     zonepy = Column(String(20))
     level = Column(Integer)
     t = Column(Integer)
@@ -23,7 +23,7 @@ class User(Base):
 
 class Hero(Base):
     __tablename__ = 'hero'
-    heroid = Column(Integer, primary_key=True)
+    heroId = Column(Integer, primary_key=True)
     displayname = Column(String(20))
     name = Column(String(20))
     title = Column(String(20))
@@ -31,9 +31,10 @@ class Hero(Base):
 
 class Battle(Base):
     __tablename__ = 'battle'
-    battleid = Column(String(20), primary_key=True)
+    battleId = Column(String(20), primary_key=True)
     type = Column(Integer)
     time = Column(String(20))
+    # details = relationship("BattleDetail",backref="battle")
 
 
 class BattleDetail(Base):
@@ -57,7 +58,7 @@ DBSession = sessionmaker(bind=engine)
 
 def insert_user(user):
     session = DBSession()
-    new_user = User(userid=user.userid, username=user.name, zonepy=user.zonepy, level=user.level, t=user.t, r=user.r)
+    new_user = User(userId=user.userid, userName=user.name, zonepy=user.zonepy, level=user.level, t=user.t, r=user.r)
     session.add(new_user)
     try:
         session.commit()
@@ -65,5 +66,33 @@ def insert_user(user):
         print("已存在用户:", user.name)
         session.close()
         return False
+    session.close()
+    return True
+
+
+def insert_battle(battle):
+    session = DBSession()
+    new_battle = Battle(battleId=battle.battleid, type=battle.type,
+                        time=battle.detail["player_game_list"][0]["start_timestamp"])
+    session.add(new_battle)
+    for x in battle.detail["player_game_list"][0]["team_lose"]["player_champions"]:
+        new_battle_detail = BattleDetail(battleId=battle.battleid, userId=x["player"]["user_id"], win=0,
+                                         heroId=x["champion"]["id"],
+                                         mvp=x["flag_mvp_carry"], K=x["total_killed"], D=x["total_death"],
+                                         A=x["total_assist"], score=x["evaluate_in_game"])
+        session.add(new_battle_detail)
+    for x in battle.detail["player_game_list"][0]["team_win"]["player_champions"]:
+        new_battle_detail = BattleDetail(battleId=battle.battleid, userId=x["player"]["user_id"], win=1,
+                                         heroId=x["champion"]["id"],
+                                         mvp=x["flag_mvp_carry"], K=x["total_killed"], D=x["total_death"],
+                                         A=x["total_assist"], score=x["evaluate_in_game"])
+        session.add(new_battle_detail)
+    try:
+        session.commit()
+    except:
+        print("插入battle%s失败" % battle.battleid)
+        session.close()
+        return False
+    print("插入battle%s成功" % battle.battleid)
     session.close()
     return True
